a0decc5f686edc6fbf3d2ee3da624a59321564c7
akez <1791189210@qq.com>
2021-07-19 14:20:06 +0800

    cors

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 	"log"
 	"weact-backend/api"
@@ -10,6 +11,12 @@ import (
 func main() {
 	fmt.Println("Hello,Gin")
 	r := gin.Default()
+	r.Use(cors.New(cors.Config{
+		AllowCredentials: true,
+		AllowAllOrigins:  true,
+		AllowMethods:     []string{"*"},
+		AllowHeaders:     []string{"*"},
+	}))
 	r.POST("/file", api.FileUpload)
 	err := r.Run()
 	if err != nil {
